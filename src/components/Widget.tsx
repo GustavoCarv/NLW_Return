8b70e67a4e4da927d1ab8b83a5e7628ad623635e
GustavoCarv <gustavo.carvalho@econverse.com.br>
2022-05-02 23:15:22 -0300

    chore: config inicial

---
+++ src/components/Widget.tsx
@@ -0,0 +1,11 @@
+import { ChatTeardropDots } from "phosphor-react"
+
+export function Widget(){ 
+
+    return (
+        <h1>
+           <ChatTeardropDots className="w-6 h-6"/>
+        </h1>
+    )
+}
+
